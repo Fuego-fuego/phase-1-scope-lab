a96b39a487143553176a31f841f9c0e6c2cc0156
nathaniEL mujesia <nathanielmujesia@gmail.com>
2023-12-13 16:37:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution in this file!
+var customerName = `bob`;
+
+// f(x) converts customerName variable to upper cases  
+
+function upperCaseCustomerName(){
+
+    customerName  = customerName.toUpperCase();
+
+    return customerName;
+}
+
+/*      setBestCustomer()
+        it should:
+    1.when called, declares a variable called bestCustomer in 
+    global scope and assigns it to be 'not bob'
+
+
+*/
+
+var bestCustomer;
+ function setBestCustomer(){
+
+    bestCustomer = 'not bob'
+
+    return bestCustomer;
+
+    
+}
+
+function overwriteBestCustomer(){
+
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = `initialValue`;
+
+
+function changeLeastFavoriteCustomer(){
+
+    leastFavoriteCustomer = `newValue`;
+
+    return leastFavoriteCustomer
+}
+
